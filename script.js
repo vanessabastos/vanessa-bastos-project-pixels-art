2ab8daa537a0100047ec5a550c0064da1c84719e
vanessabastos <vanessarsb83@gmail.com>
2021-07-11 22:55:14 -0300

    enviando desafio 8

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const cor1 = document.getElementById('cor1');
-const cor2 = document.getElementById('cor2');
-const cor3 = document.getElementById('cor3');
-const cor4 = document.getElementById('cor4');
+const selecionaCor= document.getElementById("color-palette");
+let corDoMomento="black";
+function mudaCor(){
+    selecionaCor.addEventListener('click',function(event){
+        let corSelecionada=document.querySelector(".selected");
+        corSelecionada.classList.remove("selected");
+        event.target.classList.add("selected");
+        corDoMomento=event.target.id;
+    }
+    )
+   
+} 
+mudaCor();
 
-function addClassSelected(evento) {
-    const currentSelected = document.querySelector('.selected');
-    currentSelected.classList.remove('selected');
-    evento.target.classList.add('selected');
-}
 
-
-
+function colorPixel(){
+   let pixel = document.querySelectorAll('.pixel');
+  for(let index= 0; index < pixel.length; index += 1){
+   pixel[index].addEventListener('click', function(){
+   pixel[index].style.backgroundColor = corDoMomento; 
+   });
+   }
+   }
+   colorPixel(); 
